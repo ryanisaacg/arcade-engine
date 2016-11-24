7daa8e3349fa3348a98b0c443bef21c42fb691f2
Ryan Goldstein <ryanisaacg@gmail.com>
2016-11-24 16:54:10 -0500

    Defined a shape structure that unifies an interface for rectangles, circles, and polygons

---
+++ include/shape.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "circ.h"
+#include "lines.h"
+#include "poly.h"
+#include "rect.h"
+
+typedef struct {
+	union {
+		Rect r;
+		Circ c;
+		Polygon p;
+	} data;
+	enum { SHAPE_IS_RECT, SHAPE_IS_CIRC, SHAPE_IS_POLY} type;
+} Shape;
+
+Shape shape_rect(Rect r);
+Shape shape_circ(Circ c);
+Shape shape_poly(Polygon p);
+void set_position(Shape *s, Vector2 position);
+Vector2 get_position(Shape s);
+bool shape_contains(Shape s, Vector2 point);
+bool shape_intersects(Shape s, Line line);
---
+++ src/shape.c
@@ -0,0 +1,64 @@
+#include "shape.h"
+
+Shape shape_rect(Rect r) {
+	Shape s;
+	s.data.r = r;
+	s.type = SHAPE_IS_RECT;
+	return s;
+}
+Shape shape_circ(Circ c) {
+	Shape s;
+	s.data.c = c;
+	s.type = SHAPE_IS_CIRC;
+	return s;
+}
+Shape shape_poly(Polygon p) {
+	Shape s;
+	s.data.p = p;
+	s.type = SHAPE_IS_POLY;
+	return s;
+}
+void set_position(Shape *s, Vector2 position) {
+	switch(s->type) { 
+		case SHAPE_IS_RECT:
+			s->data.r.x = position.x;
+			s->data.r.y = position.y;
+		case SHAPE_IS_CIRC: 
+			s->data.c.x = position.x;
+			s->data.c.y = position.y;
+		case SHAPE_IS_POLY:
+			s->data.p.pos = position;
+	}
+}
+Vector2 get_position(Shape s) {
+	switch(s.type) { 
+		case SHAPE_IS_RECT:
+			return vec2_new(s.data.r.x, s.data.r.y);
+		case SHAPE_IS_CIRC: 
+			return vec2_new(s.data.c.x, s.data.c.y);
+		case SHAPE_IS_POLY:
+			return s.data.p.pos;
+	}
+}
+bool shape_contains(Shape s, Vector2 point) {
+	switch(s.type) { 
+		case SHAPE_IS_RECT:
+			return rect_contains(s.data.r, point);
+		case SHAPE_IS_CIRC: 
+			return circ_contains(s.data.c, point);
+		case SHAPE_IS_POLY:
+			return poly_contains(s.data.p, point);
+	}
+	return false;
+}
+bool shape_intersects(Shape s, Line line) {
+	switch(s.type) { 
+		case SHAPE_IS_RECT:
+			return rect_intersects(s.data.r, line);
+		case SHAPE_IS_CIRC: 
+			return circ_intersects(s.data.c, line);
+		case SHAPE_IS_POLY:
+			return poly_intersects(s.data.p, line);
+	}
+	return false;
+}
