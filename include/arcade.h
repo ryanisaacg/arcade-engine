e563faf524240b9c19591a6ce89d99ce1fd6e0af
Ryan Goldstein <ryanisaacg@gmail.com>
2016-11-28 01:08:47 -0500

    Added a single header that includes the rest of the relevant headers

---
+++ include/arcade.h
@@ -0,0 +1,10 @@
+#include "array_list.h" //from data-structures
+#include "circ.h"
+#include "collision.h"
+#include "lines.h"
+#include "poly.h"
+#include "quadtree.h"
+#include "rect.h"
+#include "shape.h"
+#include "tilemap.h" //from data-structures
+#include "world.h"
