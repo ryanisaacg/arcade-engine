871dacd9f4792b8ca4fb7a4b789d7a8022ce61a4
EpicPieGuy <ryanisaacg@gmail.com>
2016-12-25 19:04:52 -0500

    Removed the first attempt to create the library

--- include/atlas.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "shaders.h"
-
-typedef struct {
-	int width, height;
-} TextureAtlas;
-
-TextureAtlas atlas_new(char *filename, Shader shader);
--- include/shaders.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <GL/glew.h>
-
-typedef GLuint Shader;
-
-Shader load_shader(char *vertex_filename, char *fragment_filename);
