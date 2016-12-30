5ffa797d320495fb2ecce4ff369beb0d9d67b3c6
Ryan Goldstein <ryanisaacg@gmail.com>
2016-12-30 11:56:27 -0500

    Implemented 2-dimensional transformations at matrices

--- include/transform.h
+++ include/transform.h
@@ -3,16 +3,16 @@
 #include "matrix.h"
 
 typedef struct {
-	Matrix2 matrix;
-	float rotation, scale;
-	Vector2 position;
+	Matrix3 matrix;
+	float rotation;
+	Vector2 scale, position;
 } Transform;
 
-Transform trans_new(Vector2 position, float rotation, float scale);
 Transform trans_new_identity();
+Transform trans_new(Vector2 position, float rotation, Vector2 scale);
 Transform trans_new_rotation(float angle);
 Transform trans_new_translation(Vector2 translate);
-Transform trans_new_scale(float factor);
+Transform trans_new_scale(Vector2 factor);
 Transform trans_append(Transform dest, Transform source);
-Transform trans_apply(Transform source, Vector2 target);
+Vector2 trans_apply(Transform source, Vector2 target);
 
--- src/transform.c
+++ src/transform.c
@@ -3,17 +3,51 @@
 #include <math.h>
 
 Transform trans_new_identity() {
-	return (Transform) { mat2_new(1, 0, 1, 0), 0, 1, vec2_new(0, 0) };
+	return (Transform) { mat3_new_identity(), 0, vec2_new(1, 1), vec2_new(0, 0) };
 }
 
 Transform trans_new_rotation(float angle) {
 	float c = cos(angle * M_PI / 180);
 	float s = sin(angle * M_PI / 180);
-	return (Transform) { mat2_new(c, -s, s, c), angle, 1, vec2_new(0, 0) };
+	float array[3][3] = {
+		{c, -s, 0},
+		{s, c, 0},
+		{0, 0, 1}};
+	return (Transform) { mat3_new(array), angle, vec2_new(1, 1), vec2_new(0, 0) };
 }
 
-Transform trans_new_translation(Vector2 translate);
-Transform trans_new_scale(float factor);
-Transform trans_new(Vector2 position, float rotation, float scale);
-Transform trans_append(Transform dest, Transform source);
-Transform trans_apply(Transform source, Vector2 target);
+Transform trans_new_translation(Vector2 translate) {
+	float array[3][3] = {
+		{1, 0, translate.x},
+		{0, 1, translate.y},
+		{0, 0, 1}};
+	return (Transform) { mat3_new(array), 0, vec2_new(1, 1), translate };
+}
+
+Transform trans_new_scale(Vector2 factor) {
+	float array[3][3] = {
+		{factor.x, 0, 0},
+		{0, factor.y, 0},
+		{0, 0, 1}};
+	return (Transform) { mat3_new(array), 0, factor, vec2_new(0, 0) };
+}
+
+Transform trans_new(Vector2 position, float rotation, Vector2 scale) {
+	Transform p, r, s;
+	p = trans_new_translation(position);
+	r = trans_new_rotation(rotation);
+	s = trans_new_scale(scale);
+	return trans_append(r, trans_append(s, p));
+}
+
+Transform trans_append(Transform dest, Transform source) {
+	Matrix3 result = mat3_mult_mat3(dest.matrix, source.matrix);
+	float rotation = dest.rotation + source.rotation;
+	Vector2 scale = vec2_add(dest.scale, source.scale);
+	Vector2 position = vec2_add(dest.position, source.position);
+	return (Transform) { result, rotation, scale, position };
+}
+
+Vector2 trans_apply(Transform source, Vector2 target) {
+	return mat3_mult_vec2(source.matrix, target);
+}
