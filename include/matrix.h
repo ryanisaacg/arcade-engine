5943eba29f2146f7aa3a398d08f5f099dcd1bce2
Ryan Goldstein <ryanisaacg@gmail.com>
2016-12-30 11:55:42 -0500

    Switched to 3x3 matrices to make translation matrices possible

--- include/matrix.h
+++ include/matrix.h
@@ -2,15 +2,19 @@
 
 #include "lines.h"
 
-/*
- * a b
- * c d
- */
+typedef struct {
+	float x, y, z;
+} Vector3;
+
+Vector3 vec3_new(float x, float y, float z);
+float vec3_dot(Vector3 a, Vector3 b);
 
 typedef struct {
-	float a, b, c, d;
-} Matrix2;
+	float data[3][3];
+} Matrix3;
 
-Matrix2 mat2_new(float a, float b, float c, float d);
-Matrix2 mat2_mult_mat2(Matrix2 m, Matrix2 n);
-Vector2 mat2_mult_vec2(Matrix2 m, Vector2 n);
+Matrix3 mat3_new_identity();
+Matrix3 mat3_new(float data[3][3]);
+Matrix3 mat3_mult_mat3(Matrix3 m, Matrix3 n);
+Vector3 mat3_mult_vec3(Matrix3 m, Vector3 n);
+Vector2 mat3_mult_vec2(Matrix3 m, Vector2 n);
--- src/matrix.c
+++ src/matrix.c
@@ -1,21 +1,49 @@
 #include "matrix.h"
+#include <string.h>
 
-Matrix2 mat2_new(float a, float b, float c, float d) {
-	return (Matrix2) { a, b, c, d };
+Vector3 vec3_new(float x, float y, float z) {
+	return (Vector3) { x, y, z };
 }
 
-Matrix2 mat2_mult_mat2(Matrix2 m, Matrix2 n) {
-	Vector2 top 	= vec2_new(m.a, m.b);
-	Vector2 bot 	= vec2_new(m.c, m.d);
-	Vector2 left 	= vec2_new(n.a, n.c);
-	Vector2 right 	= vec2_new(n.b, n.d);
-	return mat2_new(vec2_dot(top, left), vec2_dot(top, right), 
-					vec2_dot(bot, left), vec2_dot(bot, right));
+float vec3_dot(Vector3 a, Vector3 b) {
+	return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
+Matrix3 mat3_new_identity() {
+	float in[3][3] = {
+		{1, 0, 0},
+		{0, 1, 0},
+		{0, 0, 1}};
+	return mat3_new(in);
+}
 
-Vector2 mat2_mult_vec2(Matrix2 m, Vector2 n) {
-	Vector2 top = vec2_new(m.a, m.b);
-	Vector2 bot = vec2_new(m.c, m.d);
-	return vec2_new(vec2_dot(top, n), vec2_dot(bot, n));
+Matrix3 mat3_new(float in[3][3]) {
+	Matrix3 mat;
+	memcpy(mat.data, in, 9 * sizeof(float));
+	return mat;
+}
+
+Matrix3 mat3_mult_mat3(Matrix3 m, Matrix3 n) {
+	Matrix3 result;
+	for(int i = 0; i < 3; i++) {
+		for(int j = 0; j < 3; j++) {
+			Vector3 a = vec3_new(m.data[i][0], m.data[i][1], m.data[i][2]);
+			Vector3 b = vec3_new(n.data[0][j], n.data[1][j], n.data[2][j]);
+			result.data[i][j] = vec3_dot(a, b);
+		}
+	}
+	return result;
+}
+
+Vector3 mat3_mult_vec3(Matrix3 m, Vector3 n) {
+	float result[3];
+	for(int i = 0; i < 3; i++) {
+		result[i] = vec3_dot(vec3_new(m.data[i][0], m.data[i][1], m.data[i][2]), n);
+	}
+	return vec3_new(result[0], result[1], result[2]);
+}
+
+Vector2 mat3_mult_vec2(Matrix3 m, Vector2 n) {
+	Vector3 result = mat3_mult_vec3(m, vec3_new(n.x, n.y, 1));
+	return vec2_new(result.x, result.y);
 }
