b854e087df1830959509ff08037e41299b07e1fb
Ryan Goldstein <ryanisaacg@gmail.com>
2016-12-31 18:18:47 -0500

    Started implementing the draw call interface

---
+++ src/draw-call.c
@@ -0,0 +1,29 @@
+#include "draw-call.h"
+
+DrawCall dc_new() {
+	DrawCall call;
+	call.vertex_buffer = al_new(sizeof(GLfloat));
+	call.element_buffer = al_new(sizeof(GLuint));
+	call.elements = 0;
+	return call;
+}
+
+void dc_clear(DrawCall *call) {
+	al_clear(&call->vertex_buffer);
+	al_clear(&call->element_buffer);
+	call->elements = 0;
+}
+
+void dc_add_manual(DrawCall *call, GLfloat *vertices, size_t vertex_size, 
+									GLuint *elements, size_t num_elements) {
+
+}
+
+void dc_add(DrawCall *call, Rect area, Transform transform) {
+
+}
+
+void dc_draw(DrawCall call) {
+
+}
+
