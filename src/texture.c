566fbf00cb22d369f145b7d1f396914dea1e495d
EpicPieGuy <ryanisaacg@gmail.com>
2017-01-09 17:43:20 -0500

    Implemented the SDL2 image loading function

--- src/texture.c
+++ src/texture.c
@@ -1,8 +1,25 @@
 #include "texture.h"
 
-Texture texture_new(Window window, char *texture_path) {
-}
+#include <SDL_image.h>
+#include <stdio.h>
 
-void texture_destroy(Texture tex) {
-	SDL_DestroyTexture(tex.texture);
+Texture texture_new(Window window, char *texture_path) {
+	SDL_Texture *tex = NULL;
+	SDL_Surface *loaded = IMG_Load(texture_path);
+	if(loaded == NULL) {
+		goto err;
+	} else {
+		tex = SDL_CreateTextureFromSurface(window.rend, loaded);
+		if(tex == NULL) {
+			goto err;
+		}
+		SDL_FreeSurface(loaded);
+	}
+	Texture texture;
+	texture.texture = tex;
+	SDL_QueryTexture(tex, NULL, NULL, &(texture.width), &(texture.height));
+	return texture;
+	err:
+	fprintf(stderr, "Unable to load image %s. \n\tSDL Error: %s\n\tSDL_Image Error: %s", texture_path, SDL_GetError(), IMG_GetError());
+	exit(-1);
 }
