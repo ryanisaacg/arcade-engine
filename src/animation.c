ae410eed0e36b239f69492c0027fd818a0d4ac9d
ryanisaacg <ryan@ryanisaacg.com>
2017-01-11 08:12:23 -0500

    Added a function to create an animation from a spritesheet

--- src/animation.c
+++ src/animation.c
@@ -9,6 +9,17 @@ Animation anim_new(TextureRegion *frames, size_t num_frames, size_t steps_per_fr
 	};
 }
 
+Animation anim_new_spritesheet(TextureRegion source, int tile_width, int tile_height, int frame_order[][2], size_t frames, size_t steps) {
+	TextureRegion *frame_buffer = malloc(sizeof(TextureRegion) * frames);
+	for(size_t i = 0; i < frames; i++) {
+		int x = frame_order[i][0] * tile_width;
+		int y = frame_order[i][1] * tile_height;
+		TextureRegion frame = texregion_slice(source, rect_new(x, y, tile_width, tile_height));
+		frame_buffer[i] = frame;
+	}
+	return anim_new(frame_buffer, frames, steps);
+}
+
 TextureRegion anim_get_current(Animation anim) {
 	TextureRegion *current = al_get(anim.frames, anim.current_frame);
 	return *current;
