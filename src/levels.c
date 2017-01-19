78cb7f72afbce854205453cf53064a7e6a964aa2
ryanisaacg <ryan@ryanisaacg.com>
2017-01-19 10:43:46 -0500

    Got most of objects loaded from TMX files

--- src/levels.c
+++ src/levels.c
@@ -6,9 +6,9 @@
 //The side of a quad tree bucket square at the smallest level
 #define QT_BUCKETS_SIZE 64
 
-static void load_layer(World *world, tmx_layer *current);
-static void load_object_layer(World *world, tmx_layer *current);
-static void load_image_layer(World *world, tmx_layer *current);
+static void load_layer(AssetManager assets, World *world, tmx_map *map, tmx_layer *current);
+static void load_object_layer(AssetManager assets, World *world, tmx_map *map, tmx_layer *current);
+static void load_image_layer(AssetManager assets, World *world, tmx_map *map, tmx_layer *current);
 
 Level level_load(char *filename, AssetManager assets, size_t data_size) {
 	tmx_map *map = tmx_load(filename);
@@ -28,23 +28,23 @@ Level level_load(char *filename, AssetManager assets, size_t data_size) {
 		case L_NONE:
 			break;
 		case L_LAYER:
-			load_layer(&world, current_layer);
+			load_layer(assets, &world, map, current_layer);
 			break;
 		case L_OBJGR:
-			load_object_layer(&world, current_layer);	
+			load_object_layer(assets, &world, map, current_layer);	
 			break;
 		case L_IMAGE:
-			load_image_layer(&world, current_layer);
+			load_image_layer(assets, &world, map, current_layer);
 			break;
 		}
 	}
 }
 
-static void load_layer(World *world, tmx_layer *current) {
+static void load_layer(AssetManager assets, World *world, tmx_map *map, tmx_layer *current) {
 
 }
 
-static void load_object_layer(World *world, tmx_layer *current) {
+static void load_object_layer(AssetManager assets, World *world, tmx_map *map, tmx_layer *current) {
 	tmx_object *obj = current->content.objgr->head;
 	while(obj != NULL) {
 		float x = obj->x + current->offsetx;
@@ -72,11 +72,22 @@ static void load_object_layer(World *world, tmx_layer *current) {
 		if(obj->rotation != 0) {
 			shape_set_rotation(&shape, obj->rotation);
 		}
+		tmx_tile *tile = tmx_get_tile(map, obj->gid);
+		Sprite sprite;
+		if(tile->animation) {
+			TextureRegion *frames = malloc(sizeof(TextureRegion) * tile->animation_len);
+		} else {
+			Texture tex = asset_load(assets, tile->image->source);
+			TextureRegion region = texregion_new_sized(tex, rect_new(tile->ul_x, tile->ul_y, tile->image->width, tile->image->height));
+			sprite = spr_new_static(region, shape_get_position(shape));
+		}
+		//TODO: Loading of custom properties with function pointer
+		world_add(world, arcobj_new(shape, false, sprite), NULL); //TODO: load if the object shoud be solid from the TMX file 
 		obj = obj->next;
 	}
 }
 
-static void load_image_layer(World *world, tmx_layer *current) {
+static void load_image_layer(AssetManager assets, World *world, tmx_map *map, tmx_layer *current) {
 
 }
 
