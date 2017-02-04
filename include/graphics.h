16ffb4801b725793cf43248ff336d792db9dc1a8
Ryan Goldstein <ryanisaacg@gmail.com>
2017-02-03 21:58:49 -0500

    Converts documentation in graphics.h to doxygen
    Mostly this involved just turning the // of C++-style-comments to
    Doxygen's ///

--- include/graphics.h
+++ include/graphics.h
@@ -6,67 +6,74 @@
 #include "hashmap.h"
 
 #include "geom.h"
-//Represents a texture
+
+/**
+ * \file graphics.h
+ * Defines an interface to 2D hardware-accelerated graphics
+ */
+
+///Represents a loaded image file
 typedef struct Texture {
-	int width, height; //the width and height of the texture
+	int width; ///The width of the texture
+	int height; ///The height of the texture
 	SDL_Texture *texture; //the SDL texture backing
 } Texture;
-//A piece of a texture, used for drawing
+///A rectangular piece of a Texture that can be drawn
 typedef struct TextureRegion {
-	Texture source; //the source of the texture region
-	Rect region; //the part to draw
+	Texture source; ///the Texture that the region is a slice of
+	Rect region; ///the area of the texture to draw
 } TextureRegion;
-//Store an animation that can be drawn to the screen
+///Store an animation that can be drawn to the screen
 typedef struct Animation {
-	ArrayList frames; //An ArrayList of TextureRegions
-	size_t current_steps; //The number of steps that have passed in the current 
-	size_t steps_per_frame; //The total number of steps until the frame moves on
-	size_t current_frame; //The index of the current frame
+	ArrayList frames; ///An ArrayList of TextureRegion s
+	size_t current_steps; ///The number of steps that have passed in the current frame
+	size_t steps_per_frame; ///The total number of steps until the frame moves on
+	size_t current_frame; ///The index of the current frame
 } Animation;
-//A game sprite in the game world
+///A game sprite in the game world
 typedef struct Sprite {
 	union {
 		Animation anim;
 		TextureRegion tex;
-	} src; //Store the drawable backing as either animated or static
-	bool dynamic; //if the image is dynamic (animated) or static
-	Vector2 position, origin, scale; //the physical properties
-	float angle; //the rotation
-	bool flip_x, flip_y; //whether to flip the texture or animation
+	} src; ///Store the drawable backing as either animated or static
+	bool dynamic; ///if the image is dynamic (animated) or static
+	Vector2 position, origin, scale; ///the physical properties
+	float angle; ///the rotation
+	bool flip_x, flip_y; ///whether to flip the texture or animation
 } Sprite;
-//A set of configuration options for creating a window
+///A set of configuration options for creating a window
 typedef struct WindowConfig {
 	bool resizable, fullscreen, borderless;
 	int width, height;
 	const char *title;
 } WindowConfig;
-//The state of the mouse buttons
+///The state of the mouse buttons
 typedef struct MouseState {
 	bool left, right, middle, x1, x2, wheel_up, wheel_down;
 } MouseState;
-//Represent an sdl window and attached ancillary items
+///Represent an sdl window and attached ancillary items
 typedef struct Window {
-	SDL_Window *window; //the backing window
-	SDL_Renderer *rend; //hardware renderer attached to window
-	bool stay_open; //if the window should remain open
-	MouseState mouse, prev_mouse; //mouse input state
-	bool *keys, *prev_keys; //keyboard input state
-	Uint32 frame_start; //the clock time at which the frame started
-	int width, height; //the width and height of the window
+	SDL_Window *window; ///the backing window
+	SDL_Renderer *rend; ///hardware renderer attached to window
+	bool stay_open; ///if the window should remain open
+	MouseState mouse, prev_mouse; ///mouse input state
+	bool *keys, *prev_keys; ///keyboard input state
+	Uint32 frame_start; ///the clock time at which the frame started
+	int width, height; ///the width and height of the window
 } Window;
-//A struct to store loaded textures to allow simple memory management and disable texture duplication
+///A struct to store loaded textures to allow simple memory management and disable texture duplication
 typedef struct AssetManager {
-	Window window; //The window to use to load textures
-	HashMap *data; //The hashmap to store the texture data in
+	Window window; ///The window to use to load textures
+	HashMap *data; ///The hashmap to store the texture data in
 } AssetManager;
-//Determine what is drawn to the screen
+///Determine what is drawn to the screen
 typedef struct Camera {
-	Rect game_area; //The area of the world to draw
-	Window *window; //The Window that is being drawn to
-	bool letterbox; //If the camera should respond to mismatched aspect ratios by using bars
-	int offset_x; //The amount all projections should be offset on the x axis
-	int offset_y; //The amount all projections should be offset on the y axis
-	Vector2 scale; //The scale that will be applied to all images
+	Rect game_area; ///The area of the world to draw
+	Window *window; ///The Window that is being drawn to
+	bool letterbox; ///If the camera should respond to mismatched aspect ratios by using bars
+	int offset_x; ///The amount all projections should be offset on the x axis
+	int offset_y; ///The amount all projections should be offset on the y axis
+	Vector2 scale; ///The scale that will be applied to all images
 } Camera;
 
 // *** TEXTURES ***
@@ -77,85 +84,85 @@ TextureRegion texregion_new_sized(Texture source, Rect region);
 TextureRegion texregion_slice(TextureRegion region, Rect subregion);
 
 // *** ANIMATIONS ***
-// Creates a new Animation from a buffer
+/// Creates a new Animation from a buffer
 Animation anim_new(TextureRegion *frames, size_t num_frames, size_t steps_per_frame);
-// Creates an animation from a spritesheet
-//Frame Order is an array of points where the point is on the grid of sides tile_width, tile_height
+/// \brief Creates an animation from a spritesheet
+///
+///Frame Order is an array of points where the point is on the grid of sides tile_width, tile_height
 Animation anim_new_spritesheet(TextureRegion source, int tile_width, int tile_height, int frame_order[][2], size_t frames, size_t steps_per_frame);
-// Creates an animation from a spritesheet laid out left to right sequentially
+/// Creates an animation from a spritesheet laid out left to right sequentially
 Animation anim_new_linear_sheet(TextureRegion source, int tile_width, size_t steps_per_frame);
-// Get the current frame of the animation
+/// Get the current frame of the animation
 TextureRegion anim_get_current(Animation anim);
-// Step the animation forward and return the current frame
+/// Step the animation forward and return the current frame
 TextureRegion anim_step(Animation *anim);
 
 // *** SPRITES *** 
-//Create a sprite that only uses an image
+///Create a sprite that only uses an image
 Sprite spr_new_static(TextureRegion region, Vector2 pos);
-//Create a sprite that uses an animation
+///Create a sprite that uses an animation
 Sprite spr_new_animated(Animation anim, Vector2 pos);
-//Get the current image of the sprite
+///Get the current image of the sprite
 TextureRegion spr_image(Sprite spr);
-//Steps the sprite forward a frame
+///Steps the sprite forward a frame
 void spr_step(Sprite *spr);
-//Center the rotational origin
+///Center the rotational origin
 void spr_center_origin(Sprite *spr);
 
 // *** WINDOWING ***
-// Pack attributes into a configuration struct
+/// Pack attributes into a configuration struct
 WindowConfig window_config_new(int width, int height, const char *title);
-// Spawn a window with the given configuration
-// Initializes multimedia libraries and spawns an SDL window
+/// Spawn a window with the given configuration (Initializes multimedia libraries and spawns an SDL window)
 Window window_new(WindowConfig config);
-// Runs the event loop and check for keyboard and mouse events
-// Will update the current and previous input state
-// Will also check to see if the Window should close
+/// Runs the event loop and check for keyboard and mouse events
+/// Will update the current and previous input state
+/// Will also check to see if the Window should close
 void window_events(Window *window);
-// Start drawing with a clear color given by r, g, b (bound from 0 to 255)
+/// Start drawing with a clear color given by r, g, b (bound from 0 to 255)
 void window_start_draw(Window *window, int r, int g, int b);
-// End the draw call (present the draw to the screen)
-// Will sleep spare time to finish the frame to maintain 60 FPS
+/// Draw the sprites to the screen (Will sleep spare time to finish the frame to maintain 60 FPS)
 void window_end_draw(Window window);
-// If the Window has received a close event
+/// If the Window has received a close event
 bool window_should_contine(Window window);
-// Checks to see if a given key is currently held down
-// Key constants given by SDL key constants
+/// Checks to see if a given key is currently held down (Key constants given by SDL key constants)
 bool window_key_pressed(Window window, int key_code);
-// Checks to see if a given mouse button is currently held down
-// Button constants given by SDL constants
+/// Checks to see if a given mouse button is currently held down (Button constants given by SDL constants)
 bool window_mouse_pressed(Window window, int button);
-// Checks to see if a key was held last frame
+/// Checks to see if a key was held last frame
 bool window_key_was_pressed(Window window, int key_code);
-// Checks to see if a button was held last frame
+/// Checks to see if a button was held last frame
 bool window_mouse_was_pressed(Window window, int button);
-// Gets the mouse position in screen coordinates
+/// Gets the mouse position in screen coordinates
 Vector2 window_get_mouse_pos(Window window);
-// Draw a Sprite to the Window
-// Camera is an optional camera argument (NULL = no camera)
+/// Draw a Sprite to the Window
+/// Camera is an optional camera argument (NULL = no camera)
 void window_draw(Window window, Camera *cam, struct Sprite sprite);
-// Cleans up the Window and all multimedia libraries
+/// Cleans up the Window and all multimedia libraries
 void window_destroy(Window window);
 
 
 // *** ASSETS ***
-//Create a new asset manager using that window
+///Create a new asset manager using that window
 AssetManager asset_new(Window window);
-//Load or retrieve a raw texture from a file
+///Load or retrieve a raw texture from a file
 Texture asset_load(AssetManager assets, char *path);
-//Destroy the manager and the loaded textures
+///Destroy the manager and the loaded textures
 void asset_destroy(AssetManager assets);
 
 
 // *** CAMERA ***
-//Create a new camera
+///Create a new camera
 Camera cam_new(Window *window, Rect viewport, bool letterbox);
-//Project a point from game coordinates to the screen
+///Project a point from game coordinates to the screen
 SDL_Point cam_project_point(Camera cam, Vector2 point);
-//Unproject a point from the screen to the game
+///Unproject a point from the screen to the game
 Vector2 cam_unproject_point(Camera cam, SDL_Point screen);
-//Project a rectangle from the game to the screen
+///Project a rectangle from the game to the screen
 SDL_Rect cam_project_rect(Camera cam, Rect game_area);
-//Unproject a rectangle from the screen to the game
+///Unproject a rectangle from the screen to the game
 Rect cam_unproject_rect(Camera cam, SDL_Rect screen);
-//Update a camera's offset and scale; must be called after Window is resized or Camera is changed
+/**\brief Update a camera's offset and scale
+ *
+ * Must be called after Window is resized or Camera is changed
+ */
 void cam_update(Camera *cam);
