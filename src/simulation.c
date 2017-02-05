4abb849b863f9cb5f0809698b4bf5e02f782a7f1
EpicPieGuy <ryanisaacg@gmail.com>
2017-02-05 16:02:40 -0500

    Implements loading TMX object layers

--- src/simulation.c
+++ src/simulation.c
@@ -150,39 +150,30 @@ static void load_layer(AssetManager assets, World *world, tmx_map *map, tmx_laye
 static void load_object_layer(AssetManager assets, World *world, tmx_map *map, tmx_layer *current) {
 	tmx_object *obj = current->content.objgr->head;
 	while(obj != NULL) {
-		float x = obj->x + current->offsetx;
-		float y = obj->y + current->offsety;
-		Shape shape;
 		switch(obj->shape) {
+		case S_TILE:
+			break;
 		case S_NONE:
-			break;
 		case S_SQUARE:
-			shape = shape_rect(rect_new(x, y, obj->width, obj->height));
-			break;
 		case S_POLYLINE:
-			fprintf(stderr, "Attempted to load a Polyline object from a TMX file. Polyline objects are not supported (obj name %s)", obj->name);
-			break;
 		case S_POLYGON:
-			//TODO: Add support for loading polygonal objects from Tiled
-			break;
 		case S_ELLIPSE:
-			if(obj->width != obj->height) {
-				fprintf(stderr, "Attempted to load a non-circular ellipse from a TMX file. Only circular ellipses are perimtted (obj name %s)", obj->name);
-			}
-			shape = shape_circ(circ_new(x, y, obj->width / 2));
+			fprintf(stderr, "Attempted to load a geometric object from a TMX file. These are not supported.");
 			break;
 		}
-		if(obj->rotation != 0) {
-			shape_set_rotation(&shape, obj->rotation);
-		}
-		tmx_tile *tile = tmx_get_tile(map, obj->gid);
 		Sprite sprite;
+		Shape shape = shape_rect(rect_new(obj->x + current->offsetx, obj->y + current->offsety, obj->width, obj->height));
+		tmx_tile *tile = tmx_get_tile(map, obj->gid);
 		if(tile->animation) {
 			TextureRegion *frames = malloc(sizeof(TextureRegion) * tile->animation_len);
 		} else {
 			Texture tex = asset_load_texture(assets, tile->image->source);
 			TextureRegion region = texregion_new_sized(tex, rect_new(tile->ul_x, tile->ul_y, tile->image->width, tile->image->height));
 			sprite = spr_new_static(region, shape_get_position(shape));
+		}
+		if(obj->rotation != 0) {
+			shape_set_rotation(&shape, obj->rotation);
+			sprite.angle = obj->rotation;
 		}
 		//TODO: Loading of custom properties with function pointer
 		world_add(world, arcobj_new(shape, false, sprite), NULL); //TODO: load if the object shoud be solid from the TMX file 
@@ -195,7 +186,8 @@ static void load_image_layer(AssetManager assets, World *world, tmx_map *map, tm
 }
 
 World level_get_world(Level level) {
-
+	//TODO: Deep copy if the level is not persistent
+	return level.data;
 }
 
 void level_destroy(Level level) {
